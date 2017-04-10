2bde0bd7f653859f63371dbba57e6b652bbedeb8
ipc103 <iancandysemail@gmail.com>
2017-04-10 15:35:12 -0400

    add rails app

---
+++ student_lister/config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+
+  get '/courses', to: 'courses#index', as: 'courses'
+  get '/courses/something_else', to: 'courses#something_else'
+  #resources :courses, only: [:index]
+  resources :dogs
+
+  # The following is defined by resources
+  # index
+  # get '/students', to: 'students#index', as: 'students'
+  # # new
+  # get '/students/new', to: 'students#new'
+  # #show
+  # get '/students/:id', to: 'students#show', as: 'student'
+  # #edit
+  # get '/students/:id/edit', to: 'students#edit'
+  # # create
+  # post '/students', to: 'students#create'
+  # # update
+  # patch '/students/:id', to: 'students#update'
+  # # delete
+  # delete '/students/:id', to: 'students#destroy'
+
+  resources :students, except: [:new]
+  get '/register', to: 'students#new', as: 'new_student'
+
+end
